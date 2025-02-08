44f7fdb13856ad052f0b24e3a7138dd05e57f4d8
Lucus-M <lucusmulhorn@gmail.com>
2025-02-08 13:06:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
-    <div class="tile selected">
-        <img src="sampleImgs/tile1.png">
-    </div>
-*/
-
 let tileSelector = document.getElementById("tileSelector");
 
 let tileSize = {
@@ -11,12 +5,46 @@ let tileSize = {
     y: 16
 }
 
+let scale = 1;
+
 let numOfTiles = 3;
 let tileSelectorArray = new Array(numOfTiles);
 let selectedTile = 0;
 
 let tileImages = new Array(numOfTiles);
 let tileImgSrcs = new Array(numOfTiles);
+
+let cvs = document.getElementById("mainCanvas");
+
+//cvs.getContext("2d").imageSmoothingEnabled = false;
+
+let ctx = cvs.getContext("2d");
+let cvsx;
+let cvsy;
+
+
+resize();
+
+function resize(){
+    let containerWidth = document.getElementById("canvasContainer").offsetWidth;
+    let containerHeight = document.getElementById("canvasContainer").offsetHeight;
+
+    //console.log(containerWidth);
+    //console.log(containerHeight);
+
+    cvs.width = containerWidth - (containerWidth % tileSize.x);
+    cvs.height = containerHeight - (containerHeight % tileSize.y);
+
+    //console.log(cvs.width);
+    //console.log(cvs.height);
+
+    cvsy = cvs.getBoundingClientRect().y;
+    cvsx = cvs.getBoundingClientRect().x;
+
+    //console.log(`CvsY: ${cvsy}, CvsX: ${cvsx}`);
+}
+
+
 
 function select(i){
     tileSelectorArray[selectedTile].element.classList.remove("selected");
@@ -60,13 +88,6 @@ for(let i = 0; i < numOfTiles; i++){
     tileSelector.appendChild(tileSelectorArray[i].element);
 }
 
-let cvs = document.getElementById("mainCanvas");
-let ctx = cvs.getContext("2d");
-
-let cvsx;
-let cvsy;
-
-resize();
 
 let tileArray = new Array(cvs.height / tileSize.y);
 
@@ -77,36 +98,28 @@ for(let y = 0; y < tileArray.length; y++){
     }
 }
 
+function rescale(){
+    cvs.width = (tileSize.x * scale) * tileArray[0].length;
+    cvs.height = (tileSize.y * scale) * tileArray.length;
+    
+    document.getElementById("scaleDisplay").innerText = "Scale = " + scale.toFixed(1);
+
+    redrawCanvas();
+}
+
 console.log(tileArray);
 
 console.log(`CvsY: ${cvsy}, CvsX: ${cvsx}`);
 
-function resize(){
-    let containerWidth = document.getElementById("canvasContainer").offsetWidth;
-    let containerHeight = document.getElementById("canvasContainer").offsetHeight;
-
-    //console.log(containerWidth);
-    //console.log(containerHeight);
-
-    cvs.width = containerWidth - (containerWidth % tileSize.x);
-    cvs.height = containerHeight - (containerHeight % tileSize.y);
-
-    //console.log(cvs.width);
-    //console.log(cvs.height);
-
-    cvsy = cvs.getBoundingClientRect().y;
-    cvsx = cvs.getBoundingClientRect().x;
-
-    //console.log(`CvsY: ${cvsy}, CvsX: ${cvsx}`);
-}
-
 function redrawCanvas(){
+    ctx.imageSmoothingEnabled = false;
+
     ctx.clearRect(0, 0, cvs.width, cvs.height);
     
     for(let y = 0; y < tileArray.length; y++){
         for(let x = 0; x < tileArray[y].length; x++){
             if(tileArray[y][x] != 0){
-                ctx.drawImage(tileSelectorArray[tileArray[y][x]].img, x*tileSize.x, y*tileSize.y);
+                ctx.drawImage(tileSelectorArray[tileArray[y][x]].img, (x*tileSize.x) * scale, (y*tileSize.y) * scale, tileSize.x*scale, tileSize.y*scale);
             }
         }
     }
@@ -115,11 +128,17 @@ function redrawCanvas(){
 let X, y, tileX, tileY, clicking;
 
 function updateMousePos(event){
+    cvsy = cvs.getBoundingClientRect().y;
+    cvsx = cvs.getBoundingClientRect().x;
+
     x = event.clientX;
     y = event.clientY;
 
-    tileX = Math.floor((x-cvsx)/tileSize.x);
-    tileY = Math.floor((y-cvsy)/tileSize.y);
+    tileX = Math.floor((x-cvsx)/(tileSize.x*scale));
+    tileY = Math.floor((y-cvsy)/(tileSize.x*scale));
+
+    console.log(cvs.getBoundingClientRect().x);
+    console.log(cvs.getBoundingClientRect().y);
 }
 
 function updateTile(ty, tx){
@@ -137,9 +156,13 @@ cvs.addEventListener('mousemove', (event) => {
     redrawCanvas();
 
     //cursor
+    if(selectedTile != 0){
+        ctx.drawImage(tileSelectorArray[selectedTile].img, tileX*tileSize.x*scale, tileY*tileSize.y*scale, tileSize.x*scale, tileSize.y*scale);
+    }
+
     ctx.beginPath();
     ctx.strokeStyle = "white";
-    ctx.rect(tileX*tileSize.x, tileY*tileSize.y, tileSize.x, tileSize.y);
+    ctx.rect(tileX*tileSize.x*scale, tileY*tileSize.y*scale, tileSize.x*scale, tileSize.y*scale);
     ctx.stroke();
     //console.log(`Mouse X: ${tileX}, Mouse Y: ${tileY}`);
 });
@@ -153,5 +176,16 @@ cvs.addEventListener('mousedown', (event) => {
 cvs.addEventListener('mouseup', function(){
     clicking = false;
 })
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == "="){
+        scale += 0.1;
+        rescale();
+    }
+    if(event.key == "-"){
+        scale -= 0.1;
+        rescale();
+    }
+});
 
 redrawCanvas();
